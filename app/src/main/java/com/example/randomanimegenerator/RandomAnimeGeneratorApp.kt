1813ed1f464e473a1dd3424b98b9d65edc0a9600
sebastianmartii <sebastian.marzec02@gmail.com>
2023-10-10 17:01:57 +0200

    extend Image Loader Factory, implement new Image Loader with memory and dick cache, cross-fade set to true and respect cache headers to false

--- app/src/main/java/com/example/randomanimegenerator/RandomAnimeGeneratorApp.kt
+++ app/src/main/java/com/example/randomanimegenerator/RandomAnimeGeneratorApp.kt
@@ -1,7 +1,30 @@
 package com.example.randomanimegenerator
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RandomAnimeGeneratorApp : Application()
+class RandomAnimeGeneratorApp : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(filesDir.resolve("image_cache"))
+                    .maxSizeBytes(512L * 1024 * 1024)
+                    .build()
+            }
+            .crossfade(true)
+            .respectCacheHeaders(false)
+            .build()
+    }
+}
