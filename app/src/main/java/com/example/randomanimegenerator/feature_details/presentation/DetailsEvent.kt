4242855401a0eb56fa45f1fee30f58fddafcca90
sebastianmartii <sebastian.marzec02@gmail.com>
2023-10-16 20:38:12 +0200

    delete navigate to destination and navigate back details events, change navigation to single review event and add event for resetting spectated review state parameter

--- app/src/main/java/com/example/randomanimegenerator/feature_details/presentation/DetailsEvent.kt
+++ app/src/main/java/com/example/randomanimegenerator/feature_details/presentation/DetailsEvent.kt
@@ -1,6 +1,7 @@
 package com.example.randomanimegenerator.feature_details.presentation
 
 import coil.memory.MemoryCache
+import com.example.randomanimegenerator.feature_details.domain.model.Review
 import com.example.randomanimegenerator.feature_generator.presentation.Type
 import com.example.randomanimegenerator.feature_library.presentation.LibraryStatus
 
@@ -8,16 +9,8 @@ sealed interface DetailsEvent {
     data class GenerateRecommendationsAndStaff(val type: Type) : DetailsEvent
     data class AddOrRemoveFromFavorites(val isFavorite: Boolean) : DetailsEvent
     data class SelectStatus(val status: LibraryStatus) : DetailsEvent
-    data class NavigateToDestination(val destination: String) : DetailsEvent
-    data class NavigateToSingleReview(
-        val destination: String,
-        val author: String,
-        val score: Int,
-        val review: String
-    ) : DetailsEvent
-
-    data class NavigateToRecommendation(val destination: String, val malId: Int) : DetailsEvent
-    object NavigateBack : DetailsEvent
+    data class NavigateToSingleReview(val review: Review) : DetailsEvent
+    object NavigateBackFromSingleReview : DetailsEvent
     data class ShowPopUpImage(val placeholder: MemoryCache.Key?) : DetailsEvent
     object ExpandSynopsis : DetailsEvent
 }
